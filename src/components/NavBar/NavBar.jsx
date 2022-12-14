7a620bb1bd2191902f0b4942d7b1bcb9c2caeba5
brosari0 <Trumpetskickbrass86@gmail.com>
2022-12-14 19:00:51 +0000

    navbar redesign

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import './NavBar.css'
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,13 +9,16 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+    <nav className='NavBar'>
+      <img src="https://cdn-icons-png.flaticon.com/128/3913/3913866.png" alt="🎼" />
+      <span className='title'>Orpheus</span>
+      <ul>
+      <li><Link to="/create">Create</Link></li>
+      <li><Link to="/feed">Feed</Link></li>
+      <li><Link to="/community">Community</Link></li>
+      <li><Link to="/support">support</Link></li>
+      <li className='logout'><Link to="" onClick={handleLogOut}>Log Out</Link></li>
+      </ul>
     </nav>
   );
 }
