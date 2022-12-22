a7ae325aae21a83428eabb597555d5245b21f9a2
brosari0 <Trumpetskickbrass86@gmail.com>
2022-12-22 00:04:11 +0000

    Minor improvement to feed page

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -6,7 +6,7 @@ export default function FeedPage({ posts }) {
 
   return (
     <div className="structure">
-    <h1>Feed Page</h1>
+    <h1>Welcome to Orpheus! Listen to some wonderful people below! </h1>
     {posts.map((post, idx) => (
         <div className="FeedPage">
           <FeedList post={post} key={post._id} />
