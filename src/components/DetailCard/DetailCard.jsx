355076c9d5ff723b8f75eececc7add373e94967a
brosari0 <Trumpetskickbrass86@gmail.com>
2023-03-03 20:06:32 +0000

    delete button will show when the user that made the post is logged in

--- src/components/DetailCard/DetailCard.jsx
+++ src/components/DetailCard/DetailCard.jsx
@@ -3,6 +3,7 @@ import CommentCard from "../CommentCard/CommentCard";
 import * as commentsAPI from "../../utilities/comments-api";
 import "./DetailCard.css";
 import Footer from "../Footer/Footer";
+import * as postsAPI from "../../utilities/posts-api";
 
 export default function DetailCard({ user, post, updatePost }) {
   const [newComment, setNewComment] = useState('')
@@ -15,13 +16,18 @@ export default function DetailCard({ user, post, updatePost }) {
   }
 
   
-  // async function handleDelete() {
-  //   await postsAPI.deletePost(post._id); 
-  // }
+  async function handleDelete() {
+    await postsAPI.deletePost(post._id); 
+  }
 
   return (
   <>
       <div className="detail-body">
+        {user.name === post.user.name ? 
+          <button>Delete</button>
+          :
+          ''
+        }
         <div className="detail-video-aside">
           {
             post.url.includes("mp4") ?
@@ -33,34 +39,31 @@ export default function DetailCard({ user, post, updatePost }) {
           {post.title}
           </h4>
         </div>
-      </div>
-    <aside className="detail-aside">
-      <p className="detail-description">
-        {post.description}
-      </p>
-    </aside>
-      <div>
-        <h3>Comment Section:</h3>
-        {user ? 
-        <form className="detail-form" onSubmit={handleSubmit}>
-          <label>Make a New Comment:</label>
-          <input required type="text" value={newComment} onChange={(evt) => setNewComment(evt.target.value)} />
-          <button type="submit">Submit</button>
-        </form>
-        :
-        ''
-        }
+        <aside className="detail-aside">
+          <p className="detail-description">
+            {post.description}
+          </p>
+        </aside>
         <div>
-          { 
-            post.comments.map((comment) => (
-            <CommentCard comment={comment} key={comment._id} />
-          ))}
-
-        </div> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
-        {/* Temporary until I fix the css for the footer ^ */}
-        <Footer /> 
-
+          <h3>Comment Section:</h3>
+          {user ? 
+          <form className="detail-form" onSubmit={handleSubmit}>
+            <label>Make a New Comment:</label>
+            <input required type="text" value={newComment} onChange={(evt) => setNewComment(evt.target.value)} />
+            <button type="submit">Submit</button>
+          </form>
+          :
+          ''
+          }
+          <div>
+            { 
+              post.comments.map((comment) => (
+              <CommentCard comment={comment} key={comment._id} />
+            ))}
+          </div>
+        </div>
       </div>
+        <Footer /> 
     </>
   )
 }
